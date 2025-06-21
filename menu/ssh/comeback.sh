1e95d3d85ecfccba2b798a08209699c6a578b0cd
NIKU1323 <puputjaya883@gmail.com>
2025-06-22 01:34:28 +0700

    Create comeback.sh

---
+++ menu/ssh/comeback.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# BACK TO MAIN MENU - NIKU CLOUD
+
+clear
+echo -e "\e[32m──────────────────────────────────────────\e[0m"
+echo -e "\e[32m        KEMBALI KE MENU UTAMA VPN         \e[0m"
+echo -e "\e[32m──────────────────────────────────────────\e[0m"
+sleep 1
+bash /root/menu/menu.sh
